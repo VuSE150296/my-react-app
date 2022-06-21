4545d06f55fefed68100a822730f38594ed7c76f
VuSE150296 <danglehoaivu@gmail.com>
2022-06-21 20:19:15 +0700

    EX26

--- src/App.js
+++ src/App.js
@@ -3,26 +3,15 @@ import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 
 import React, { Component } from 'react';
-import { DISHES } from './shared/dishes';
-import DishDetail from './component/DishdetailComponent';
+import Main from './component/MainComponent'
 
 // import PropTypes from 'prop-types';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { dishes: DISHES };
-  }
-
   render() {
     return (
       <div className='App'>
-        <Navbar dark color='primary'>
-          <div className='component'>
-            <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <DishDetail dishes={this.state.dishes} />
+        <Main />
       </div>
 
     );
@@ -30,44 +19,3 @@ class App extends Component {
 }
 
 export default App;
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className='App'>
-//         <Navbar dark color='primary'>
-//           <div className='component'>
-//             <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
-//           </div>
-//         </Navbar>
-//         <Menu />
-//       </div>
-
-//     );
-//   }
-// }
-
-// export default App;
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-// 
-// export default App;
--- src/component/DishdetailComponent.js
+++ src/component/DishdetailComponent.js
@@ -2,39 +2,26 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class DishDetail extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedDish: null
-        };
-    }
-
-    onDishSelect(dish) {
-        this.setState({ selectedDish: dish });
-    }
     renderDish(dish) {
-        if (dish != null)
-            return (
+        return (
+            <div className="col-12 col-md-5 m-1">
                 <Card>
                     <CardImg top src={dish.image} alt={dish.name} />
                     <CardBody>
-                        <CardTitle><h4>{dish.name}</h4></CardTitle>
-                        <CardText><h6>{dish.description}</h6></CardText>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
                     </CardBody>
                 </Card>
-            );
-        else
-            return (
-                <div></div>
-            );
+            </div>
+        );
     }
-    renderComments(dish) {
-        if (dish != null)
+    renderComments(comments) {
+        if (comments != null)
             return (
-                <div className='text-left card-deck ml-1 h-[540px]'>
+                <div className='text-left col-12 col-md-5 m-2'>
                     <h3 className='mb-3'>Comments</h3>
                     <div>
-                        {dish.comments.map((item) => {
+                        {comments.map((item) => {
                             var date = new Date(item.date);
                             return (
                                 <div key={item.id} className='mb-3 font-weight-bold'>
@@ -55,34 +42,21 @@ class DishDetail extends Component {
     }
 
     render() {
-        const menu = this.props.dishes.map((dish) => {
+        if (this.props.dish != null) {
             return (
-                <div className="col-12 col-md-5 m-1">
-                    <Card key={dish.id}
-                        onClick={() => this.onDishSelect(dish)}>
-                        <CardImg width="100%" src={dish.image} alt={dish.name} />
-                        <CardImgOverlay>
-                            <CardTitle className="text-left font-weight-bold">{dish.name}</CardTitle>
-                        </CardImgOverlay>
-                    </Card>
+                <div className="container">
+                    <div className="row">
+                        {this.renderDish(this.props.dish)}
+                        {this.renderComments(this.props.dish.comments)}
+                    </div>
                 </div>
             );
-        });
-        return (
-            <div className='container'>
-                <div className='row'>
-                    {menu}
-                </div>
-                <div className='row'>
-                    <div className='col-12 col-md-5 m-1'>
-                        {this.renderDish(this.state.selectedDish)}
-                    </div>
-                    <div className='col-12 col-md-5 m-1'>
-                        {this.renderComments(this.state.selectedDish)}
-                    </div>
-                </div>
-            </div>
-        );
+        }
+        else {
+            return (
+                <div></div>
+            );
+        }
     }
 }
 
