59867110edb20413a5c96bb8567dd6a0ab3c5555
VuSE150296 <danglehoaivu@gmail.com>
2022-07-05 04:53:02 +0700

    Controlled Form Validation 2

--- src/component/ContactComponent.js
+++ src/component/ContactComponent.js
@@ -8,7 +8,6 @@ import {
   Label,
   Input,
   Col,
-  Row,
   FormFeedback,
 } from "reactstrap";
 import { Link } from "react-router-dom";
@@ -33,8 +32,8 @@ class Contact extends Component {
       },
     };
 
-    this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
     this.handleBlur = this.handleBlur.bind(this);
   }
 
@@ -54,7 +53,7 @@ class Contact extends Component {
     event.preventDefault();
   }
 
-  handleBlur = (field) => (event) => {
+  handleBlur = (field) => (evt) => {
     this.setState({
       touched: { ...this.state.touched, [field]: true },
     });
@@ -66,17 +65,21 @@ class Contact extends Component {
       telnum: "",
       email: "",
     };
+
     if (this.state.touched.firstname && firstname.length < 3)
       errors.firstname = "First Name should be >= 3 characters";
     else if (this.state.touched.firstname && firstname.length > 10)
       errors.firstname = "First Name should be <= 10 characters";
+
     if (this.state.touched.lastname && lastname.length < 3)
       errors.lastname = "Last Name should be >= 3 characters";
     else if (this.state.touched.lastname && lastname.length > 10)
       errors.lastname = "Last Name should be <= 10 characters";
-    const reg = "/^d+$/";
-    if (this.state.touched.telnum && !reg.telnum(telnum))
+
+    const reg = /^\d+$/;
+    if (this.state.touched.telnum && !reg.test(telnum))
       errors.telnum = "Tel. Number should contain only numbers";
+
     if (
       this.state.touched.email &&
       email.split("").filter((x) => x === "@").length !== 1
